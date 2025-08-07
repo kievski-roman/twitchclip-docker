6093c7d3cfdb542cf14cacf3a8377b38bd84de76
kievski-roman <rkievski@gmil.com>
2025-08-07 11:45:09 +0200

    CI action

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -7,7 +7,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? 'SubGenius' }}</title>
-    @vite(['resources/css/app.css', 'resources/js/alpine.js','resources/js/theme.js'])
+
+    @if (file_exists(public_path('build/manifest.json')))
+        @vite(['resources/css/app.css', 'resources/js/alpine.js','resources/js/theme.js'])
+    @endif
 </head>
 <body
     class="bg-gradient-to-br from-gray-100 via-purple-100 to-blue-100">
